84d8582a26d80042d14f5979c6f9721dc401d94b
KAUE NUNES <106996986+KAUEn40@users.noreply.github.com>
2023-10-30 14:08:47 +0000

    hahahaahha

---
+++ main3.js
@@ -0,0 +1,14 @@
+const listadeTeclas = document.querySelectorAll
+function invocado(){
+    console.log('Não pegou')
+    listadeTeclas;
+}
+kaued('.tecla_pom').onclick = invocado;
+kaued('.tecla_clap').onclick = invocado;
+kaued('.tecla_tim').onclick = invocado;
+kaued('.tecla_puff').onclick = invocado;
+kaued('.tecla_splash').onclick = invocado;
+kaued('.tecla_toim').onclick = invocado;
+kaued('.tecla_psh').onclick = invocado;
+kaued('.tecla_tic').onclick = invocado;
+kaued('.tecla_tom').onclick = invocado;
